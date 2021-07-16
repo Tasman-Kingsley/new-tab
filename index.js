5cd600b3e65d385bb04b75ccb5a4bba7540fd08f
tasman-kingsley <tasman.kingsley@gmail.com>
2021-07-16 23:14:51 +1000

    v0.23

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 function search() {
   const sInput = $('#searchInput').val();
-  window.open(`https://www.google.com/search?q=${sInput}&btnG=Search`, '_self');
+  if ($('#goo').prop('checked')) {
+    window.open(`https://www.google.com/search?q=${sInput}&btnG=Search`, '_self');
+  } else {
+    window.open(`https://duckduckgo.com/?q=${sInput}&t=h_&ia=web`, '_self');
+  }
 }
+
+document.addEventListener('keydown', (event) => {
+  if (event.keyCode === 13) {
+    search();
+  }
+});
+
+// https://duckduckgo.com/?q=jung&t=h_&ia=web
