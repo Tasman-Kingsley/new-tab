459c375d958770bf3dc3c20193637bd44278371d
tasmankingsley <tasman.kingsley@gmail.com>
2022-02-23 12:25:15 +1100

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()]
-})
+  base: '/new-tab/',
+  plugins: [svelte()],
+  build: {
+    outDir: 'docs',
+  },
+});
